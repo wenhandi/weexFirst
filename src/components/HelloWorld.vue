17f46fac6e66931671fbeada8edca525cf79164c
许盼 <xupan03@missfresh.cn>
2019-01-30 17:31:30 +0800

    test

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,3 +1,45 @@
 <template>
-  <text class="message">Now, let's use Vue.js to build your Weex app.</text>
+  <div style="justify-content:center">
+    <text class="label">Vue.js Star Count</text>
+    <text class="count">{{count}}</text>
+  </div>
 </template>
+
+<script>
+  const stream = weex.requireModule('stream')
+  export default {
+    data () {
+      return {
+        count: 'fetching...'
+      }
+    },
+    created () {
+      stream.fetch({
+        method: 'GET',
+        type: 'json',
+        url: 'https://api.github.com/repos/vuejs/vue'
+      }, res => {
+        if (res.ok) {
+          this.count = res.data.stargazers_count
+        } else {
+          this.count = '- unknown -'
+        }
+      })
+    }
+  }
+</script>
+
+<style scoped>
+  .label {
+    color: #666;
+    text-align: center;
+    font-size: 60px;
+  }
+  .count {
+    color: #41B883;
+    text-align: center;
+    font-size: 100px;
+    margin-top: 80px;
+    margin-bottom: 100px;
+  }
+</style>
